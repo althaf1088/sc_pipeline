724834827481d776969e24095cba20a7b3a628bf
Althaf <althaf1088@gmail.com>
2019-07-25 21:38:01 +0800

    terraform format

--- terraform/autoscale.tf
+++ terraform/autoscale.tf
@@ -1,24 +1,27 @@
 ## Creating Launch Configuration
 resource "aws_launch_configuration" "terra_web" {
-  image_id               = "${var.webservers_ami}"
-  instance_type          = "t2.micro"
+  image_id        = "${var.webservers_ami}"
+  instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.webservers.id}"]
-  user_data = "${file("user_data.sh")}"
+  user_data       = "${file("user_data.sh")}"
+
   lifecycle {
     create_before_destroy = true
   }
 }
+
 ## Creating AutoScaling Group
 resource "aws_autoscaling_group" "terra_web_asg" {
   launch_configuration = "${aws_launch_configuration.terra_web.id}"
-  availability_zones = ["${element(var.azs,count.index)}"]
-  min_size = 2
-  max_size = 3
-  load_balancers = ["${aws_elb.terra-elb.name}"]
-  health_check_type = "ELB"
+  availability_zones   = ["${element(var.azs,count.index)}"]
+  min_size             = 2
+  max_size             = 3
+  load_balancers       = ["${aws_elb.terra-elb.name}"]
+  health_check_type    = "ELB"
+
   tag {
-    key = "Name"
-    value = "terraform-asg-example"
+    key                 = "Name"
+    value               = "terraform-asg-example"
     propagate_at_launch = true
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,19 @@
 variable "aws_region" {
-	default = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-	default = "10.20.0.0/16"
+  default = "10.20.0.0/16"
 }
 
 variable "subnets_cidr" {
-	type = "list"
-	default = ["10.20.1.0/24", "10.20.2.0/24"]
+  type    = "list"
+  default = ["10.20.1.0/24", "10.20.2.0/24"]
 }
 
 variable "azs" {
-	type = "list"
-	default = ["us-east-1a", "us-east-1b"]
+  type    = "list"
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "webservers_ami" {
