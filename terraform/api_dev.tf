ed131f64edf07a63b5fbc4980fd11ed824b18fc9
Althaf <althaf1088@gmail.com>
2019-07-28 04:58:33 +0800

    get ip

--- terraform/api_dev.tf
+++ terraform/api_dev.tf
@@ -67,7 +67,7 @@ resource "aws_elb" "terra_elb" {
 
 #AMI
 resource "aws_ami_from_instance" "wp_golden" {
-  name               = "sc-ami-1ff8a11517f17f867"
+  name               = "sc-ami-2ff8a11517f17f867"
   source_instance_id = "${aws_instance.webservers.id}"
 
 }
