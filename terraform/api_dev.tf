d4f976fbbe39dc9cc55ab4aad5d6ee9c4c28ced6
Althaf <althaf1088@gmail.com>
2019-07-28 17:33:40 +0800

    get ip

--- terraform/api_dev.tf
+++ terraform/api_dev.tf
@@ -65,7 +65,7 @@ resource "aws_elb" "terra_elb" {
 
 #AMI
 resource "aws_ami_from_instance" "wp_golden" {
-  name               = "sc-ami-8ff1a82517f17f167"
+  name               = "sc-ami-8ff1a82510f17f167"
   source_instance_id = "${aws_instance.webservers.id}"
 
 }
@@ -96,10 +96,10 @@ resource "aws_launch_configuration" "terra_web_lc" {
 resource "aws_autoscaling_group" "terra_web_asg" {
   name                      = "asg-${aws_launch_configuration.terra_web_lc.id}"
   max_size                  = 1
-  min_size                  = 1
+  min_size                  = 2
   health_check_grace_period = "300"
   health_check_type         = "EC2"
-  desired_capacity          = 1
+  desired_capacity          = 2
   force_delete              = true
   load_balancers            = ["${aws_elb.terra_elb.id}"]
   availability_zones   = ["${element(var.azs,count.index)}"]
