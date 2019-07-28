9ead48c3bc9d1699a874443644e5a03e70b48b0a
Althaf <althaf1088@gmail.com>
2019-07-28 17:43:35 +0800

    change autoscaler capacity

--- terraform/api_dev.tf
+++ terraform/api_dev.tf
@@ -65,7 +65,7 @@ resource "aws_elb" "terra_elb" {
 
 #AMI
 resource "aws_ami_from_instance" "wp_golden" {
-  name               = "sc-ami-8ff1a82510f17f167"
+  name               = "sc-ami-8ff1a83510f17f167"
   source_instance_id = "${aws_instance.webservers.id}"
 
 }
@@ -95,8 +95,8 @@ resource "aws_launch_configuration" "terra_web_lc" {
 
 resource "aws_autoscaling_group" "terra_web_asg" {
   name                      = "asg-${aws_launch_configuration.terra_web_lc.id}"
-  max_size                  = 1
-  min_size                  = 2
+  max_size                  = 2
+  min_size                  = 1
   health_check_grace_period = "300"
   health_check_type         = "EC2"
   desired_capacity          = 2
