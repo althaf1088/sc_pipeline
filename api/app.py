19deeb8b22e9db831aa364377df35f1ff754f0c8
Althaf <althaf1088@gmail.com>
2019-07-28 13:44:35 +0800

    get ip

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123nextstar@database-1.cxwoepvqqboq.us-east-2.rds.amazonaws.com:5432/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123nextstar@terraform-20190728053629924800000001.cxhwmrxzgirv.us-east-1.rds.amazonaws.com:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = '123star'
--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -3,5 +3,5 @@ sudo yum update
 cd /home/ec2-user/
 source flask/bin/activate
 cd /home/ec2-user/api
-export DATABASE_URL="postgresql://postgres:123nextstar@database-1.cxwoepvqqboq.us-east-2.rds.amazonaws.com:5432/postgres"
+export DATABASE_URL="postgresql://postgres:123nextstar@terraform-20190728053629924800000001.cxhwmrxzgirv.us-east-1.rds.amazonaws.com:5432/postgres"
 gunicorn -b "0.0.0.0:8000" run:app --daemon
